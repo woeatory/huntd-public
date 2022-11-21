b1b2f4c713ab9ac590ea6d8beb977f9ac02ab5ef
Volodymyr Kryvytskyi <vovakrivickiy34@gmail.com>
2022-11-21 19:33:47 +0200

    fixed authUser

--- api/src/modules/user/user.resolvers/authUser.resolver.ts
+++ api/src/modules/user/user.resolvers/authUser.resolver.ts
@@ -1,4 +1,4 @@
-import { GetAuthUser } from '@/modules/user/user.useCases/GetAuthUser.useCase';
+import { GetAuthUserUseCase } from '@/modules/user/user.useCases/GetAuthUser.useCase';
 import { makeResolver } from '@/core';
 
-export const authUserResolver = makeResolver(GetAuthUser);
+export const authUserResolver = makeResolver(GetAuthUserUseCase);
